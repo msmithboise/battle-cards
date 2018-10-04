a41ca34b008fee1da9306ee63e60d3d8cc0201f7
Michael Smith <msmithboise@gmail.com>
2018-10-04 15:20:13 -0600

    yup

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  baseUrl: 'https://msmithboise.github.io/battle-cards/',
-  outputDir: 'docs'
+  baseUrl: 'https://msmithboise.github.io/battle-cards/docs',
+  outputDir: 'docs',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
